19e2e4ca03118369b8fe00fe157639edf3c5e399
Raymond Yegon <raymondyegon17@gmail.com>
2022-08-02 11:22:48 +0300

    Change main app entry point

---
+++ lib/features/app/app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ge_wallet/features/app/app_tabs.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => AnnotatedRegion<SystemUiOverlayStyle>(
+        value: const SystemUiOverlayStyle(
+          statusBarIconBrightness: Brightness.light, //Android
+          statusBarBrightness: Brightness.dark, //iOS
+        ),
+        child: MaterialApp(
+          title: 'Waves Wallet',
+          builder: (context, child) => MediaQuery(
+            data: MediaQuery.of(context).copyWith(
+              textScaleFactor: 1.0,
+            ),
+            child: child!,
+          ),
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const AppTabs(),
+        ),
+      );
+}
---
+++ lib/features/app/app_tabs.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:ge_wallet/core/presentation/page_margin.dart';
+
+class AppTabs extends StatefulWidget {
+  const AppTabs({Key? key}) : super(key: key);
+
+  @override
+  State<AppTabs> createState() => _AppTabsState();
+}
+
+class _AppTabsState extends State<AppTabs> {
+  int selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _pages = <Widget>[
+      Container(
+        child: const Center(
+          child: Text('Home'),
+        ),
+      ),
+      Container(
+        child: const Center(
+          child: Text('exchange'),
+        ),
+      ),
+      Container(
+        child: const Center(
+          child: Text('Home'),
+        ),
+      ),
+      Container(
+        child: const Center(
+          child: Text('transactions'),
+        ),
+      ),
+      Container(
+        child: const Center(
+          child: Text('settings'),
+        ),
+      ),
+    ];
+
+    return PageMargin(
+      showBottomNav: true,
+      selectedIndex: selectedIndex,
+      backgroundColor: Colors.white,
+      padding: EdgeInsets.zero,
+      onNavItemTap: (int v) {
+        if (v != 2) {
+          setState(() {
+            selectedIndex = v;
+          });
+        } else {}
+      },
+      child: IndexedStack(
+        index: selectedIndex,
+        children: _pages,
+      ),
+    );
+  }
+}
