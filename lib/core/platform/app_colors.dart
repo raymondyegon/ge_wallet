6461985d7ac99c0256dc52a710328e7cf1dd9033
Raymond Yegon <raymondyegon17@gmail.com>
2022-08-02 10:40:40 +0300

    Registered colors

---
+++ lib/core/platform/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  /// #1C99D5
+  static Color strongBlue = const Color(0xFF1C99D5);
+
+  /// #282828
+  static Color darkGray = const Color(0xFF282828);
+
+  /// #4b4b4b
+  static Color mediumDarkGray = const Color(0xFF4b4b4b);
+
+  /// #C4C4C4
+  static Color lightGray = const Color(0xFFC4C4C4);
+
+  /// #B8B8B8
+  static Color mediumLightGray = const Color(0xFFB8B8B8);
+
+  ///#bdfbe9 lime green
+  static Color limeGreen = const Color(0xFFbdfbe9);
+}
